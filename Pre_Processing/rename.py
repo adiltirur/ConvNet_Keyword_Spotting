6e56b656a235e0be19b4deedb3cfda77932830d4
adiltirur1 <adiltirur@gmail.com>
2018-03-07 20:38:11 +0100

    build_full model

--- Pre_Processing/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-path = '/home/adil/NN_Project/dataset/renamed/ten'
-files = os.listdir(path)
-i = 1
-
-for file in files:
-    os.rename(os.path.join(path, file), os.path.join(path, str(i)+'.wav'))
-    i = i+1
